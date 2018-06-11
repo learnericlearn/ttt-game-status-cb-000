16e02382e967d28d92453075bfa3cec2ddc47599
Eric Frommelt <ericfrommelt@gmail.com>
2018-06-11 16:26:10 +0000

    Done.

--- lib/game_status.rb
+++ lib/game_status.rb
@@ -4,3 +4,61 @@ def position_taken?(board, index)
 end
 
 # Define your WIN_COMBINATIONS constant
+WIN_COMBINATIONS = [
+  [0, 1, 2],
+  [3, 4, 5],
+  [6, 7, 8],
+
+  [0, 3, 6],
+  [1, 4, 7],
+  [2, 5, 8],
+
+  [0, 4, 8],
+  [2, 4, 6]
+]
+
+def won?(board)
+  WIN_COMBINATIONS.each do |win_combination|
+
+    if board[win_combination[0]] == "X" && board[win_combination[1]] == "X" && board[win_combination[2]] == "X"
+      return win_combination
+      elsif board[win_combination[0]] == "O" && board[win_combination[1]] == "O" && board[win_combination[2]] == "O"
+        return win_combination
+      end
+    end
+    return false
+end
+
+def full?(board)
+  board.all? { |player| player == "X" || player == "O" }
+end
+
+def draw?(board)
+  if won?(board)
+    return false
+  elsif full?(board)
+    return true
+  end
+end
+
+def over?(board)
+  if draw?(board)
+    return true
+  elsif won?(board)
+    return true
+  elsif full?(board)
+    return true
+  end
+end
+
+def winner(board)
+  if won?(board)
+    xcount = board.count("X")
+    ocount = board.count("O")
+    if xcount > ocount
+      return "X"
+    elsif ocount > xcount
+      return "O"
+    end
+  end
+end
