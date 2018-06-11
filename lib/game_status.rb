ca32fd16fb56c9c1c998ca8fa82e5d7efa6342c1
Eric Frommelt <ericfrommelt@gmail.com>
2018-06-11 16:28:59 +0000

    Done.

--- lib/game_status.rb
+++ lib/game_status.rb
@@ -19,7 +19,6 @@ WIN_COMBINATIONS = [
 
 def won?(board)
   WIN_COMBINATIONS.each do |win_combination|
-
     if board[win_combination[0]] == "X" && board[win_combination[1]] == "X" && board[win_combination[2]] == "X"
       return win_combination
       elsif board[win_combination[0]] == "O" && board[win_combination[1]] == "O" && board[win_combination[2]] == "O"
